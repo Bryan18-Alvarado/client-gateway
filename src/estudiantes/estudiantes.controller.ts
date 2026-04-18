9e7b358be817f2ce0b919b04f66cc4a78d975a2c
Bryan18-Alvarado <bryanalvarado1904@gmail.com>
2026-04-18 11:07:00 -0600

    refactor: reorganize imports and implement CRUD methods in EstudiantesController

--- src/estudiantes/estudiantes.controller.ts
+++ src/estudiantes/estudiantes.controller.ts
@@ -1,7 +1,20 @@
-import { Body, Controller, Inject, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Inject,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 import { ESTUDIANTES_SERVICE } from 'src/config/service';
-import { CreateEstudianteDto } from './dto/create-estudiante.dto';
+import {
+  CreateEstudianteDto,
+  UpdateEstudianteDto,
+} from './dto/create-estudiante.dto';
 
 @Controller('estudiantes')
 export class EstudiantesController {
@@ -9,11 +22,37 @@ export class EstudiantesController {
     @Inject(ESTUDIANTES_SERVICE) private readonly estudianteClient: ClientProxy,
   ) {}
 
+  @Get()
+  getAll() {
+    return this.estudianteClient.send({ cmd: 'get_all_student' }, {});
+  }
+
+  @Get(':id')
+  getOne(@Param('id', ParseIntPipe) id: number) {
+    return this.estudianteClient.send({ cmd: 'get_one_student' }, id);
+  }
+
   @Post()
   create(@Body() estudianteDto: CreateEstudianteDto) {
+    return this.estudianteClient.send({ cmd: 'create_student' }, estudianteDto);
+  }
+
+  @Put(':id')
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() estudianteDto: UpdateEstudianteDto,
+  ) {
     return this.estudianteClient.send(
-      { cmd: 'create_estudiante' },
-      estudianteDto,
+      { cmd: 'update_student' },
+      {
+        id,
+        data: estudianteDto,
+      },
     );
+  }
+
+  @Delete(':id')
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.estudianteClient.send({ cmd: 'remove_student' }, id);
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import 'dotenv/config';
 import { AppModule } from './app.module';
-
 async function bootstrap() {
   const logger = new Logger('Main-Gateway');
   const app = await NestFactory.create(AppModule);
